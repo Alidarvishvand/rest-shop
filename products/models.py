6fb0e2fd233dd3d1c87b78c60ebd8239223946af
alidv <imalidrv@gmail.com>
2024-07-28 15:05:52 +0330

    "added html page" added new class for serializer next step => complete user app

--- products/models.py
+++ products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from user.models import User
+from django.urls import reverse
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
@@ -16,7 +17,16 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     status = models.BooleanField()
-    
+    created_date = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateField(auto_now=True)
     
     def __str__(self):
         return self.title
+    
+    
+    def get_snippet(self):
+        return self.content[0:5]
+
+    def get_absolute_api_url(self):
+        return reverse("products:post-detail", kwargs={"pk": self.pk})
--- products/serializers.py
+++ products/serializers.py
@@ -1,9 +1,13 @@
-# products/serializers.py
-
 from rest_framework import serializers
-from .models import Product
+from .models import Product,Post
 
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
+        fields = '__all__'
+        
+class Postserializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Post
         fields = '__all__'
